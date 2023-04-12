316fbf2d8e765b40066981e505af389be0bfa746
stefanpython <stefanhtml@yahoo.com>
2023-04-12 18:12:05 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import "./App.css";
+import Navbar from "./components/Nav";
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <h1>aaaaaaaa</h1>
+      <Navbar />
+
+      <section id="about">
+        <h2>About Me</h2>
+        <p>
+          Insert your introduction or summary of your skills, expertise, and
+          experience here.
+        </p>
+      </section>
+
+      <section id="projects">
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <h3>Project Title 1</h3>
+            <p>Project Description 1</p>
+
+            <a href="#">View Project</a>
+          </li>
+          <li>
+            <h3>Project Title 2</h3>
+            <p>Project Description 2</p>
+            <a href="#">View Project</a>
+          </li>
+          <li>
+            <h3>Project Title 3</h3>
+            <p>Project Description 3</p>
+            <a href="#">View Project</a>
+          </li>
+        </ul>
+      </section>
+
+      <section id="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>
+            Programming Languages: List your programming language expertise
+          </li>
+          <li>
+            Tools and Technologies: List your proficiency in tools and
+            technologies
+          </li>
+          <li>
+            Soft Skills: List your interpersonal skills, communication, team
+            management, leadership, and problem-solving skills
+          </li>
+        </ul>
+      </section>
+
+      <section id="contact">
+        <h2>Contact Me</h2>
+        <form action="#" method="post">
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name" required />
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" required />
+          <label for="message">Message</label>
+          <textarea id="message" name="message" required></textarea>
+          <button type="submit">Send Message</button>
+        </form>
+      </section>
+
+      <footer>
+        <p>&copy; 2023 - Your Name</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Portfolio;
---
+++ src/components/Nav.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+
+function Navbar() {
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const about = document.getElementById("about");
+    const projects = document.getElementById("projects");
+    const skills = document.getElementById("skills");
+    const contact = document.getElementById("contact");
+
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    if (
+      contact.offsetTop <= window.pageYOffset + 50 &&
+      contact.offsetTop + contact.offsetHeight > window.pageYOffset + 50
+    ) {
+      navLinks[3].classList.add("active");
+    } else if (
+      skills.offsetTop <= window.pageYOffset + 50 &&
+      skills.offsetTop + skills.offsetHeight > window.pageYOffset + 50
+    ) {
+      navLinks[2].classList.add("active");
+    } else if (
+      projects.offsetTop <= window.pageYOffset + 50 &&
+      projects.offsetTop + projects.offsetHeight > window.pageYOffset + 50
+    ) {
+      navLinks[1].classList.add("active");
+    } else if (
+      about.offsetTop <= window.pageYOffset + 50 &&
+      about.offsetTop + about.offsetHeight > window.pageYOffset + 50
+    ) {
+      navLinks[0].classList.add("active");
+    }
+  };
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
