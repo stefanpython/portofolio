41cc8da0548ba95a8bc2b91ea17b9af9c2bc7104
stefanpython <stefanhtml@yahoo.com>
2023-04-13 14:27:19 +0300

    Add certification links and images

--- src/App.js
+++ src/App.js
@@ -53,31 +53,31 @@ function Portfolio() {
           <li>Programming Languages, Tools and Technologies used:</li>
 
           <div className="tools--icons">
-            <i class="devicon-javascript-plain colored"></i>
+            <i className="devicon-javascript-plain colored"></i>
 
-            <i class="devicon-react-original-wordmark colored"></i>
+            <i className="devicon-react-original-wordmark colored"></i>
 
-            <i class="devicon-visualstudio-plain colored"></i>
+            <i className="devicon-visualstudio-plain colored"></i>
 
-            <i class="devicon-npm-original-wordmark colored"></i>
+            <i className="devicon-npm-original-wordmark colored"></i>
 
-            <i class="devicon-firebase-plain-wordmark colored"></i>
+            <i className="devicon-firebase-plain-wordmark colored"></i>
 
-            <i class="devicon-html5-plain-wordmark colored"></i>
+            <i className="devicon-html5-plain-wordmark colored"></i>
 
-            <i class="devicon-css3-plain-wordmark colored"></i>
+            <i className="devicon-css3-plain-wordmark colored"></i>
 
-            <i class="devicon-github-original-wordmark colored"></i>
+            <i className="devicon-github-original-wordmark colored"></i>
 
-            <i class="devicon-linux-plain colored"></i>
+            <i className="devicon-linux-plain colored"></i>
 
-            <i class="devicon-nodejs-plain colored"></i>
+            <i className="devicon-nodejs-plain colored"></i>
 
-            <i class="devicon-express-original-wordmark colored"></i>
+            <i className="devicon-express-original-wordmark colored"></i>
 
-            <i class="devicon-webpack-plain colored"></i>
+            <i className="devicon-webpack-plain colored"></i>
 
-            <i class="devicon-sqlite-plain colored"></i>
+            <i className="devicon-sqlite-plain colored"></i>
           </div>
 
           <li>
@@ -89,21 +89,32 @@ function Portfolio() {
 
       <section id="certifications">
         <h2>Certifications</h2>
-        <p>...</p>
-        <p>...</p>
-        <p>...</p>
-        <p>...</p>
-        <p>...</p>
+
+        <div className="certification--container">
+          <a href="https://certificates.cs50.io/e434c454-d200-4ccd-a308-ce32fb560d22.png?size=letter">
+            <h3>CS50's Introduction to Computer Science</h3>
+            <img src="images/cs50x.png" alt="cs50 logo" />
+          </a>
+
+          <a href="https://certificates.cs50.io/0608492f-e1b9-4169-8e36-c37a61101a3d.png?size=letter">
+            <h3>CS50’s Web Programming with Python and JavaScript</h3>
+            <img src="images/cs50w.png" alt="cs50w logo" />
+          </a>
+
+          <div className="theodinproject">
+            <img src="images/top.png" alt="the odin project logo" />
+          </div>
+        </div>
       </section>
 
       <section id="contact">
         <h2>Contact Me</h2>
         <form action="#" method="post">
-          <label for="name">Name</label>
+          <label htmlFor="name">Name</label>
           <input type="text" id="name" name="name" required />
-          <label for="email">Email</label>
+          <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
-          <label for="message">Message</label>
+          <label htmlFor="message">Message</label>
           <textarea id="message" name="message" required></textarea>
           <button type="submit">Send Message</button>
         </form>
